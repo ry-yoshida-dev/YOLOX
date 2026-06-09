bde8f7851a4ed68a3684269025fa875c3f48dc92
ry-yoshida <ry-yoshida@yachiyo-eng.co.jp>
2026-06-09 04:01:47 +0000

    update init

--- src/yolox/__init__.py
+++ src/yolox/__init__.py
@@ -1,3 +1,36 @@
 """YOLOX inference-only package."""
 
+from .models import (
+    BaseConv,
+    CSPDarknet,
+    DWConv,
+    Darknet,
+    SiLU,
+    YOLOPAFPN,
+    YOLOX,
+    YOLOXHead,
+)
+from .utils import (
+    bboxes_iou,
+    cxcywh2xyxy,
+    meshgrid,
+    postprocess,
+)
+
 __version__ = "0.3.0.dev0"
+
+__all__ = [
+    "BaseConv",
+    "CSPDarknet",
+    "DWConv",
+    "Darknet",
+    "SiLU",
+    "YOLOPAFPN",
+    "YOLOX",
+    "YOLOXHead",
+    "__version__",
+    "bboxes_iou",
+    "cxcywh2xyxy",
+    "meshgrid",
+    "postprocess",
+]
